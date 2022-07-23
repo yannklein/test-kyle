b3429b6d189215102f23dac08b5d196d16daeb2f
Kyle Thomas <kylejwthomas@gmail.com>
2022-07-23 13:44:49 +0900

    skeleton setup

---
+++ src/Hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <img src="./photo-grid.png" className="hero--photo" alt="grid"/>
+            <h1 className="hero--header">Unique Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by
+            one-of-a-kind hosts, either online or in-person.</p>
+        </section>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,109 @@
+export default [
+  {
+    id: 1,
+    title: "Ninja Training",
+    description: "Learn the art of ninjutsu. Train with skilled professionals and learn how to handle ninja weapons such as shuriken, kunai, and smoke bombs.",
+    price: 100,
+    coverImg: "ninja.png",
+    stats: {
+        rating: 5.0,
+        reviewCount: 54
+    },
+    location: "Yokohama",
+    openSpots: 15,
+  },
+
+  {
+    id: 2,
+    title: "Life Lessons with Katie Zaferes",
+    description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+    price: 120,
+    coverImg: "swimmer.png",
+    stats: {
+        rating: 4.5,
+        reviewCount: 16
+    },
+    location: "Online",
+    openSpots: 0,
+  },
+
+  {
+    id: 3,
+    title: "Hike Mt. Fuji",
+    description: "Want to tackle the highest mountain in Japan? Why not join a hiking group to make the journey more enjoyable. Any level can join!",
+    price: 80,
+    coverImg: "mt-fuji.png",
+    stats: {
+        rating: 4.8,
+        reviewCount: 72
+    },
+    location: "Yamanashi",
+    openSpots: 40,
+  },
+
+{
+  id: 4,
+  title: "Kimono Lessons and Kyoto Sightseeing",
+  description: "Learn to properly wear a Japanese kimono and walk with getta. Sightsee around Kyoto in the kimono and enjoy the beautiful scenery of traditional Japan.",
+  price: 200,
+  coverImg: "kimono.png",
+  stats: {
+      rating: 4.6,
+      reviewCount: 60
+  },
+  location: "Kyoto",
+  openSpots: 10,
+},
+{
+  id: 5,
+  title: "Learn Wedding Photography",
+  description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+  price: 125,
+  coverImg: "wedding-photography.png",
+  stats: {
+      rating: 4.0,
+      reviewCount: 20
+  },
+  location: "Online",
+  openSpots: 17,
+},
+{
+  id: 6,
+  title: "Make Washi - Japanese Paper",
+  description: "Learn the art of washi making in this fun course with experienced washi making instructors. You can even get some to take home as souvenirs",
+  price: 50,
+  coverImg: "washi.png",
+  stats: {
+      rating: 3.5,
+      reviewCount: 20
+  },
+  location: "Kokubunji",
+  openSpots: 12,
+},
+{
+  id: 7,
+  title: "Japanese Calligraphy - Shodo",
+  description: "Try your hand at Shodo, also known as Japanese calligraphy. Learn how to write beautiful kanji and create masterpieces.",
+  price: 65,
+  coverImg: "shodo.png",
+  stats: {
+      rating: 4.2,
+      reviewCount: 45
+  },
+  location: "Online",
+  openSpots: 20,
+},
+{
+  id: 8,
+  title: "The Art of Ikebana",
+  description: "Create flower arrangements with style and sophistication. This course will truly test your patience and creativity",
+  price: 80,
+  coverImg: "ikebana.png",
+  stats: {
+      rating: 4.1,
+      reviewCount: 15
+  },
+  location: "Ryogoku",
+  openSpots: 5,
+},
+]
