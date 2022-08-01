6185954f8697971963a55063ecf93506ae41d4b4
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-01 14:50:02 +0900

    polished site and added more locations

--- src/data.js
+++ src/data.js
@@ -12,7 +12,7 @@ export default [
   {
     title: "Sydney Opera House",
     location: "Australia",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567841,151.2065419,15z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
     startDate: "27 May, 2021",
     endDate: "8 Jun, 2021",
     description:
@@ -22,11 +22,41 @@ export default [
   {
     title: "Geirangerfjord",
     location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1045655,6.7950426,10z/data=!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940816",
+    startDate: "01 Dec, 2021",
+    endDate: "10 Jan, 2022",
+    description:
+      "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+  },
+  {
+    title: "Eiffel Tower",
+    location: "France",
+    googleMapsUrl: "https://www.google.com/maps/place/Eiffel+Tower/@48.8583701,2.2944813,17z/data=!3m1!4b1!4m5!3m4!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0!8m2!3d48.8583701!4d2.2944813?hl=en",
     startDate: "01 Oct, 2021",
     endDate: "18 Nov, 2021",
     description:
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+    imageUrl: "https://www.swedishnomad.com/wp-content/images/2019/04/Facts-about-the-Eiffel-tower.jpg",
+  },
+  {
+    title: "Grand Canyon",
+    location: "USA",
+    googleMapsUrl: "https://www.google.com/maps/place/Grand+Canyon/@36.0997575,-112.1475041,13z/data=!3m1!4b1!4m5!3m4!1s0x80cc0654bd27e08d:0xb1c2554442d42e8d!8m2!3d36.0997631!4d-112.1124846?hl=en",
+    startDate: "20 Jan, 2022",
+    endDate: "11 Feb, 2022",
+    description:
+      "The Grand Canyon is a steep-sided canyon carved by the Colorado River in Arizona, United States. The Grand Canyon is 277 miles long, up to 18 miles wide and attains a depth of over a mile.",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Canyon_River_Tree_%28165872763%29.jpeg/800px-Canyon_River_Tree_%28165872763%29.jpeg",
+  },
+  {
+    title: "Niagara Falls",
+    location: "Canada",
+    googleMapsUrl: "https://www.google.com/maps/place/Niagara+Falls,+ON,+Canada/@43.0535908,-79.3682178,10z/data=!3m1!4b1!4m5!3m4!1s0x89d3445eec824db9:0x46d2c56156bda288!8m2!3d43.0895577!4d-79.0849436?hl=en",
+    startDate: "15 Feb, 2022",
+    endDate: "1 Mar, 2022",
+    description:
+      "Niagara Falls is a group of three waterfalls at the southern end of the Niagara Gorge, spanning the border between the province of Ontario in Canada and the state of New York in the United States.",
+    imageUrl: "https://www.planetware.com/photos-large/CDN/canada-niagara-falls-horseshoe-falls.jpg",
   },
 ];
